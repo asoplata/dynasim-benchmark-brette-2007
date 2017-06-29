18889dbf3d02baec556ad5a7b3016d25eb33ee8b
Austin Soplata <austin.soplata@gmail.com>
2017-06-29 14:55:34 -0400

    Finalize first benchmark

--- DynaSim/benchmark_CUBA_nosyn_4000.m
+++ DynaSim/benchmark_CUBA_nosyn_4000.m
@@ -1,32 +1,45 @@
 %{
+# Notes:
+
+- This simulation seeks to emulate the CUBA, no synapse simulations of Figure 5
+of (Goodman and Brette, 2008) using the DynaSim simulator for speed benchmark
+comparison.
+- The time taken to simulate will be indicated in the stdout log file
+'~/batchdirs/benchmark_CUBA_nosyn_4000/pbsout/sim_job1.out'
+
+# References:
+
+- Goodman D, Brette R. Brian: a simulator for spiking neural networks in Python.
+Frontiers in Neuroinformatics 2008;2. doi:10.3389/neuro.11.005.2008.
 %}
 
-run_name = 'benchmark_CUBA_raw';
+run_name = 'benchmark_CUBA_nosyn_4000';
 
-% Save data/results to this directory. If just a single name, will
-%   save to that directory name in the current directory from which it's run.
-%   Will create directory if it does not exist.
-data_dir = strcat(run_name);
+total_cells = 4000;
+numEcells = 0.8*total_cells;
+numIcells = 0.2*total_cells;
 
 time_end = 500; % in milliseconds
-numEcells = 3200;
-numIcells = 800;
 
 %% Create DynaSim specification structure
 s=[];
 s.populations(1).name='E';
 s.populations(1).size=numEcells;
-s.populations(1).equations={'dv/dt=-gLeak*(V-ELeak)./taum; if(v>-50)(v=-60)'};
+s.populations(1).equations={'dv/dt=-gLeak*(v-ELeak)./taum; if(v>-50)(v=-60)'};
 s.populations(1).parameters={'taum',20,'gLeak',1,'ELeak',-49};
 
 s.populations(2).name='I';
 s.populations(2).size=numIcells;
-s.populations(2).equations={'dv/dt=-gLeak*(V-ELeak)./taum; if(v>-50)(v=-60)'};
+s.populations(2).equations={'dv/dt=-gLeak*(v-ELeak)./taum; if(v>-50)(v=-60)'};
 s.populations(2).parameters={'taum',20,'gLeak',1,'ELeak',-49};
-
 
 %% Set other, non-network simulation parameters
 vary={};
+
+% Save data/results to this directory. If just a single name, will
+%   save to that directory name in the current directory from which it's run.
+%   Will create directory if it does not exist.
+data_dir = strcat(run_name);
 
 % Flags
 % How much RAM if you need MUCH RAM - options: 8G (default), 94G, 125G, 252G, 504G
@@ -34,7 +47,7 @@ memlimit =     '252G';
 cluster_flag =      1;
 overwrite_flag =    1;
 save_data_flag =    0;
-save_results_flag = 1;
+save_results_flag = 0;
 verbose_flag =      1;
 compile_flag =      0;
 disk_flag =         0;
@@ -49,9 +62,10 @@ data = dsSimulate(s,'save_data_flag',save_data_flag,'study_dir',data_dir,...
                   'memlimit',memlimit,'compile_flag',compile_flag,...
                   'disk_flag',disk_flag,'downsample_factor',downsample_factor,...
                   'benchmark_flag',benchmark_flag,...
-                  'vary',vary,...
-                  'plot_functions',{@dsPlot,@dsPlot,@dsPlot},...
-                  'plot_options',{{'plot_type','waveform','format','png'},...
-                                  {'plot_type','power','format','png'},...
-                                  {'plot_type','rastergram','format','png'}});
-% exit
+                  'plot_functions',{@dsPlot},...
+                  'plot_options',{{'plot_type','waveform','format','png'}});
+exit
+
+%% If you want to parameter sweep, add this flag:
+
+%                   'vary',vary,...
