ac6fe461d3d70bf256225ce0ca1b30abf769c141
asoplata <austin.soplata@gmail.com>
2017-08-04 14:25:33 -0400

    Make plot prettier

--- analyze_data.m
+++ analyze_data.m
@@ -3,10 +3,10 @@ data = readtable('data.csv');
 
 f1 = figure(1);
 subplot 221
-loglog(data.CellCount, data.BrianCUBA_Nosyn, 'r','DisplayName','Brian')
+loglog(data.CellCount, data.BrianCUBA_Nosyn,'-or','DisplayName','Brian')
 hold on
-loglog(data.CellCount, data.DynaCUBA_NosynNocompile, 'b','DisplayName','DynaSim Not Compiled')
-loglog(data.CellCount, data.DynaCUBA_NosynCompile, 'k','DisplayName','DynaSim Compiled')
+loglog(data.CellCount, data.DynaCUBA_NosynNocompile, '-ob','DisplayName','DynaSim Not Compiled')
+loglog(data.CellCount, data.DynaCUBA_NosynCompile, '-ok','DisplayName','DynaSim Compiled')
 hold off
 xlabel('Cell number')
 ylabel('Time in seconds')
@@ -14,20 +14,20 @@ title('CUBA No synapses')
 legend('show')
 
 subplot 222
-loglog(data.CellCount, data.BrianCOBAHH_Nosyn, 'r')
+loglog(data.CellCount, data.BrianCOBAHH_Nosyn, '-or')
 hold on
-loglog(data.CellCount, data.DynaCOBAHH_NosynNocompile, 'b')
-loglog(data.CellCount, data.DynaCOBAHH_NosynCompile, 'k')
+loglog(data.CellCount, data.DynaCOBAHH_NosynNocompile, '-ob')
+loglog(data.CellCount, data.DynaCOBAHH_NosynCompile, '-ok')
 hold off
 xlabel('Cell number')
 ylabel('Time in seconds')
 title('COBAHH No synapses')
 
 subplot 223
-loglog(data.CellCount, data.BrianCOBAHH_ClocksynLodensEstimated, 'r')
+loglog(data.CellCount, data.BrianCOBAHH_ClocksynLodensEstimated, '-or')
 hold on
-loglog(data.CellCount, data.DynaCOBAHH_ClocksynLodensNocompile, 'b')
-loglog(data.CellCount, data.DynaCOBAHH_ClocksynLodensCompile, 'k')
+loglog(data.CellCount, data.DynaCOBAHH_ClocksynLodensNocompile, '-ob')
+loglog(data.CellCount, data.DynaCOBAHH_ClocksynLodensCompile, '-ok')
 hold off
 xlabel('Cell number')
 ylabel('Time in seconds')
@@ -35,10 +35,10 @@ title('COBAHH Clock-synapses Low density')
 
 
 subplot 224
-loglog(data.CellCount, data.BrianCOBAHH_ClocksynHidensEstimated, 'r')
+loglog(data.CellCount, data.BrianCOBAHH_ClocksynHidensEstimated, '-or')
 hold on
-loglog(data.CellCount, data.DynaCOBAHH_ClocksynHidensNocompile, 'b')
-loglog(data.CellCount, data.DynaCOBAHH_ClocksynHidensCompile, 'k')
+loglog(data.CellCount, data.DynaCOBAHH_ClocksynHidensNocompile, '-ob')
+loglog(data.CellCount, data.DynaCOBAHH_ClocksynHidensCompile, '-ok')
 hold off
 xlabel('Cell number')
 ylabel('Time in seconds')
