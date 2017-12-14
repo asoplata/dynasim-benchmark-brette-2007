a71d857f7adbebc2b01cead2afd5dc7c4852c2b5
Austin Soplata <austin.soplata@gmail.com>
2017-12-14 00:16:36 -0500

    Add Brian2 compiled analysis

--- analyze_data.m
+++ analyze_data.m
@@ -5,6 +5,7 @@ f1 = figure(1);
 subplot 221
 loglog(data.CellCount, data.BrianCUBA_Nosyn,'-or','DisplayName','Brian')
 hold on
+loglog(data.CellCount, data.BrianCUBA_Nosyn_Compiled,'-og','DisplayName','Brian')
 loglog(data.CellCount, data.DynaCUBA_NosynNocompile, '-ob','DisplayName','DynaSim Not Compiled')
 loglog(data.CellCount, data.DynaCUBA_NosynCompile, '-ok','DisplayName','DynaSim Compiled')
 hold off
@@ -16,6 +17,7 @@ legend('show')
 subplot 222
 loglog(data.CellCount, data.BrianCOBAHH_Nosyn, '-or')
 hold on
+loglog(data.CellCount, data.BrianCOBAHH_Nosyn_Compiled, '-og')
 loglog(data.CellCount, data.DynaCOBAHH_NosynNocompile, '-ob')
 loglog(data.CellCount, data.DynaCOBAHH_NosynCompile, '-ok')
 hold off
@@ -26,6 +28,7 @@ title('COBAHH No synapses')
 subplot 223
 loglog(data.CellCount, data.BrianCOBAHH_ClocksynLodensEstimated, '-or')
 hold on
+loglog(data.CellCount, data.BrianCOBAHH_ClocksynLodensEstimated_Compiled, '-og')
 loglog(data.CellCount, data.DynaCOBAHH_ClocksynLodensNocompile, '-ob')
 loglog(data.CellCount, data.DynaCOBAHH_ClocksynLodensCompile, '-ok')
 hold off
@@ -37,6 +40,7 @@ title('COBAHH Clock-synapses Low density')
 subplot 224
 loglog(data.CellCount, data.BrianCOBAHH_ClocksynHidensEstimated, '-or')
 hold on
+loglog(data.CellCount, data.BrianCOBAHH_ClocksynHidensEstimated_Compiled, '-og')
 loglog(data.CellCount, data.DynaCOBAHH_ClocksynHidensNocompile, '-ob')
 loglog(data.CellCount, data.DynaCOBAHH_ClocksynHidensCompile, '-ok')
 hold off
